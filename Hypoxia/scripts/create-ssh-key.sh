8ec6027f5ae59aa24893a647791396fc0cd696db
gdschaves <gchaves@uchicago.edu>
2022-06-23 13:19:08 -0500

    Include Shell Script Modifications

--- Hypoxia/scripts/create-ssh-key.sh
+++ Hypoxia/scripts/create-ssh-key.sh
@@ -1,17 +1,20 @@
+# Go to the ssh directory
 cd ~/.ssh
 pwd
 /Users/gepolianochaves/.ssh
+# Check what exists inside of the directory
 ls
 known_hosts
-
+# Create ssh key using the email used in the Github account. 
+# In the case below, I was interested in creating a key for my uchicago e-mail, 
+# which I was using for my github account.
 ssh-keygen -o -t rsa -C "gchaves@uchicago.edu"
+### After the command is ran, its ooutputs are as follows:
 # Generating public/private rsa key pair.
 # Enter file in which to save the key (/Users/gepolianochaves/.ssh/id_rsa):
 # Enter passphrase (empty for no passphrase):
 # Enter same passphrase again:
-
-Then the output is:
-
+### After a passphrase is set, the output is as follows:
 # Your identification has been saved in /Users/gepolianochaves/.ssh/id_rsa.
 # Your public key has been saved in /Users/gepolianochaves/.ssh/id_rsa.pub.
 # The key fingerprint is:
@@ -28,14 +31,13 @@ Then the output is:
 # |            . = +|
 # |             o + |
 # +----[SHA256]-----+
-
+### After the ssh key is generated, and we list the directory, we see the following:
 ls
 id_rsa		id_rsa.pub	known_hosts
-
+### the contente of the key can be accessed as follows:
 cat id_rsa.pub
-
-Copy this content
-
+# Copy this content
+### Then we paste the key in out github repository code:
 Go to Settings on GitHub
 Go to SSH and GPG Keys
 Go to Add New SSH Key
@@ -44,7 +46,8 @@ Click the Add SSH Key button
 
 ###########################################################
 ###########################################################
-################################################### 
+############## Save the current script to the script folder
+############## in the our repository, for documentation
 ###########################################################
 ###########################################################
 
